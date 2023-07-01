5d6516356d502a34e603a28caf27498cb0a35742
hugobbi <hgugobbi@gmail.com>
2023-07-01 17:34:48 -0300

    Fixing setup script

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='SUMO-QL',
+    name='SUMO-drivers',
     version='1.1',
-    packages=['sumo_ql', 'sumo_vg'],
+    packages=['sumo_drivers', 'sumo_vg'],
     install_requires=[
         'numpy',
         'pandas',
@@ -21,7 +21,7 @@ setup(
     ],
     author='guidytz, hugobbi',
     author_email='guidytz@gmail.com, hgugobbi@gmail.com',
-    url='https://github.com/maslab-ufrgs/SUMO-QL',
+    url='https://github.com/maslab-ufrgs/SUMO-drivers',
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     description='A python code to handle Multi-agent Reinforcement Learning, where the agents are drivers, using SUMO as a microscopic traffic simulation. This code also handles the creation of a graph that links different elements of the traffic network that have similar patterns.'
